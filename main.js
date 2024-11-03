5a17ea8e9daa88977fd5eab498b912bc97266a32
ridasersif <ridasersif1@gmail.com>
2024-11-03 14:11:31 +0100

    Add function to calculate total and store it in localStorage

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
+let title = document.getElementById('title');
+let price= document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+//*************************************************************** */
+
 //get total
+function getTotal(){
+    if(price.value!=''){
+        let result=(+price.value + +taxes.value + +ads.value )
+        - +discount.value;
+        total.innerHTML = result;
+        total.style.background='#040';
+    }else{
+        total.innerHTML='';
+        total.style.background='#a00d02'
+
+    } 
+}
+//*********************************************** */
 //creat prodact
-//save localstorage
+let dataPro;
+if(localStorage.product!=null){
+    dataPro=JSON.parse(localStorage.product)
+}else{
+    dataPro=[];
+}
+submit.onclick = function(){
+    let newPro={
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value
+    }
+    dataPro.push(newPro);
+    //save localstorage
+    localStorage.setItem('product', JSON.stringify(dataPro))
+    console.log(dataPro)
+}
+
 // clear inputs
 //read
 //count
